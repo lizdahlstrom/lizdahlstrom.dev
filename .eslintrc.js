a36d104d75fb2ea1a5ce68d703087230202a49b0
Liz Dahlström <liz.dahlstrom@gmail.com>
2022-06-14 17:52:45 +0200

    Add build folder to gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:@next/next/recommended', 'plugin:react/recommended', 'airbnb'],
+  extends: [
+    'plugin:@next/next/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -13,10 +17,16 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'jsx-quotes': 'off',
+    'object-curly-newline': 'off',
     'linebreak-style': 0,
     'no-underscore-dangle': 'off',
     'react/prop-types': 0,
     '@next/next/no-img-element': 'off',
     'react/jsx-props-no-spreading': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/link-passhref': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    'implicit-arrow-linebreak': 'off',
   },
 };
